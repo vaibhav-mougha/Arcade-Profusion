4e944e9b0ed604e3a3df78d56656e89677c00315
Vaibhav Kumar Mougha <tunu1994@gmail.com>
2023-06-06 19:46:05 +0530

    Added routes for users

---
+++ server/routes/auth/auth.route.js
@@ -0,0 +1,38 @@
+const authRouter = require("express").Router();
+const {
+  registerController,
+} = require("../../controller/auth/Register.controller");
+const { body } = require("express-validator");
+const { loginController } = require("../../controller/auth/Login.controller");
+const {
+  getUserController,
+} = require("../../controller/auth/GetUsersController");
+
+authRouter.route("/register").post(
+  [
+    body("username", "Enter your Username").not().isEmpty(),
+    body("email", "Enter a vaild Email").isEmail(),
+    body("password", "Password Length must be atleast 3").isLength({
+      min: 3,
+    }),
+    body("gender", "Select your Gender").not().isEmpty(),
+    body("dob", "Select your Date of Birth").not().isEmpty(),
+  ],
+  registerController
+);
+
+authRouter.route("/login").post(
+  [
+    body("email", "Enter a vaild email").isEmail(),
+    body("password", "Password length must be atleast 3").isLength({
+      min: 3,
+    }),
+  ],
+  loginController
+);
+
+authRouter.route("/get/users").get(getUserController);
+
+module.exports = {
+  authRouter,
+};
