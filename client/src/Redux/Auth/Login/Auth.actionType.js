3d431d5fbe9899b7df83038351d18b01151b1ce7
Vaibhav Kumar Mougha <tunu1994@gmail.com>
2023-06-23 17:52:16 +0530

    Added actionType in Auth Login

---
+++ client/src/Redux/Auth/Login/Auth.actionType.js
@@ -0,0 +1,5 @@
+export const user_login_isLoading = "user/login/loading";
+export const user_login_reset = "user/login/reset";
+export const user_login_isSuccess = "user/login/isSuccess";
+export const user_login_isError = "user/login/isError";
+export const user_logout = "user/logout"
